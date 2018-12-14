a5d40d95c9e2f35ba6cf08714714a342e3db1d77
Sergi Carol <sergicarol35@gmail.com>
2018-12-14 11:23:31 +0100

    Uopdated

--- pymongo1.py
+++ pymongo1.py
@@ -1,34 +1,77 @@
 from pymongo import MongoClient
+import csv
+from bson.code import Code
 
 # server on localhost
 conn = MongoClient()
 db = conn.test    # db = conn.foo would connect to database 'foo'
+db.test.drop()
 
-db.testing.insert({"j":5, "k":6, "m":"nopqr" })
-db.testing.insert({"j":15, "k":6, "m":"nopqr" })
+support = dict()
+confidence = dict()
 
-d = {}
-d['j'] = 10
-d['k'] = 6
-for i in range(4):
-    d['a'*i] = i
-db.testing.insert(d)
 
-d = {}
-d['j'] = 8
-d['k'] = 6
-d['b'] = "Hi there!"
-db.testing.insert(d)
+def read_file(file):
+    total_length = 0
+    with open(file) as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for line in reader:
+            d = dict()
+            d['data'] = line
+            db.test.insert_one(d)
+            total_length += 1
+    return total_length
 
-docs = db.testing.find({"j":10})
 
-for doc in docs:
-    print "(j:10)", doc
+def map_reduce():
+    mapper_pairs = Code("""
+        function() {
+            for (var i = 0; i < this.data.length; i++) {
+                for (var j = i+1; j <= this.data.length; j++){
+                    emit(this.data[i] + "," + this.data[j], 1);
+                    emit(this.data[j] + "," + this.data[i], 1);
+                    }
+                }
+            }
+            """)
+    mapper_terms = Code("""
+        function() {
+            for (var i = 0; i < this.data.length; i++) {
+                emit(this.data[i],1);
+                }
+            }
+              """)
 
-for doc in db.testing.find({"k":{"$lt":7}}):
-    print "(k<7)", doc
+    reducer = Code("""
+        function(key,values) {
+            var total = 0;
+            for (var i = 0; i < values.length; i++) {
+                total += values[i];
+                }
+                return total;
+            }
+               """)
+    db.test.map_reduce(mapper_terms, reducer, "count_terms")
+    db.test.map_reduce(mapper_pairs, reducer, "count_pairs")
 
-for doc in db.testing.find({"j":{"$gt":9}}):
-    print "(j>9)", doc
 
-db.testing.remove({ "k" : 6 })
+def associations(support, confidence, n):
+
+    result_terms = db.count_pairs.find()
+    for element in result_terms:
+        print element
+
+
+def calculate_associations(total_length):
+    result_terms = db.count_terms.find()
+    result_pairs = db.count_pairs.find()
+
+    for element in result_terms:
+        print element
+
+
+if __name__ == '__main__':
+    total_length = read_file('groceries.csv')
+    print total_length
+    map_reduce()
+    calculate_associations(total_length) 
