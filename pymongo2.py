021f3e3eb73e0e7e0dcd056421ed5fff74404421
Sergi Carol <sergicarol35@gmail.com>
2018-12-14 10:22:42 +0100

    Init repo

---
+++ pymongo2.py
@@ -0,0 +1,16 @@
+from os import path
+from codecs import encode, decode
+from pymongo import MongoClient
+
+conn = MongoClient()
+db = conn.foo
+
+with open("text.txt") as f:
+    for line in f:
+        text = []    
+        for word in line.strip().split():
+          word = decode(word.strip(),'latin2','ignore')
+          text.append(word)
+        d = {}
+        d['content'] = text
+        db.corpus.insert(d)
